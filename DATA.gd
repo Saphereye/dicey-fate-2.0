426a88ddafff85a5c475024371f061ac177afad7
Saphereye <adarshdas950@gmail.com>
2022-09-17 20:24:40 +0530

    Created Template Generation Tool

--- DATA.gd
+++ DATA.gd
@@ -1,5 +1,5 @@
 extends Node
 
-var Player_Health: int = 6
+export(int, 0, 6) var Player_Health = 6
 # Difficulty goes fomr 0 to 10
-var difficulty: int = 1
+export(int, 1, 10) var difficulty = 1
---
+++ Temp/ExtractLocationData.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
+var simple_generation_data_array = []
+var excess_randomness_data_array = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	for x in range(100):
+		for y in range(100):
+			if $TileMap.get_cell(x, y) == 0:
+				simple_generation_data_array.append([x, y])
+			if $TileMap.get_cell(x, y) == 1:
+				excess_randomness_data_array.append([x, y])
+	var simple_string = ''
+	var random_string = ''
+	for i in simple_generation_data_array:
+		simple_string += str(i) + ','
+	for i in excess_randomness_data_array:
+		random_string += str(i) + ','
+	print(simple_string)
+	print()
+	print(random_string)
